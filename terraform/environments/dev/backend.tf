4b68ae111a38b5e84ec5c3da5a02aaad33254877
Maksym Romanyuk <maksym23102006@gmail.com>
2026-05-01 15:33:31 +0300

    fix(terraform): fix with use terraform fmt

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "cost-optimizer-terraform-state"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
-    encrypt        = true
+    bucket       = "cost-optimizer-terraform-state"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
